2e78a801c21570c03325ead3bc6847614c22316c
armetcal <armetcal@edu.uwaterloo.ca>
2022-01-05 14:45:23 -0800

    Added buttons

--- MICB_353/index.js
+++ MICB_353/index.js
@@ -1,7 +1,21 @@
 var random4;
-var new_cfu = NaN;
 var amt;
 var split = 0;
+
+var answer_1;
+var answer_2;
+var answer_3a;
+var answer_3b;
+var answer_4b;
+var answer_4c;
+
+var button1_value = 0;
+var button2_value = 0;
+var button3a_value = 0;
+var button3b_value = 0;
+var button4b_value = 0;
+var button4c_value = 0;
+document.getElementById('button4b').style.display = "none";
 
 function do_onclick() {
 
@@ -18,65 +32,83 @@ function do_onclick() {
     var amt_plated = [0.01, 0.02, 0.05, 0.5, 1.0];
     var random3 = Math.floor(Math.random() * amt_plated.length);
     amt = amt_plated[random3];
-    var description1 = 'You are preparing a <strong><mark>';
-    var description2 = ' serial dilution</strong></mark> of <em>Akkermansia muciniphila</em> in order to determine how many live bacteria are present in the sample. You prepare <strong><mark>';
-    var description3 = ' dilution(s)</strong></mark> and seed <strong><mark>';
-    document.getElementById("dilution_description").innerHTML = description1 + dil + description2 + num_dilutions.toString() + description3 + amt.toString() + ' ml</strong></mark> onto an agar plate.';
+    var description1 = 'You are preparing a <b>';
+    var description2 = ' serial dilution</b> of <em>Akkermansia muciniphila</em> in order to determine how many live bacteria are present in the sample. You prepare <b>';
+    var description3 = ' dilution(s)</b> and seed <b>';
+    document.getElementById("dilution_description").innerHTML = description1 + dil + description2 + num_dilutions.toString() + description3 + amt.toString() + ' ml</b> onto an agar plate.';
 
     // Calculate final dilution
     split = dil.split('/');
     split = parseInt(split[1]);
     var new_denom = split ** num_dilutions;
-    var dil_num = '1/' + new_denom.toString();
-    document.getElementById("answer1").innerHTML = dil_num.toString();
+    answer_1 = '1/' + new_denom.toString();
+    answer_1 = answer_1.toString();
 
     // Calculate final plated dilution
     var plated = new_denom / amt;
-    var plated_num = '1/' + plated.toString();
-    document.getElementById("answer2").innerHTML = plated_num.toString();
+    answer_2 = '1/' + plated.toString();
+    answer_2 = answer_2.toString();
 
     // Calculate CFU/ml
     random4 = Math.floor(Math.random() * 400);
-    var sample_type = ['ORIGINAL', 'FINAL DILUTED'];
-    var random5 = Math.round(Math.random());
-    var sample_type = sample_type[random5];
-    var description4 = '3. After 3 days of anaerobic incubation, your plate has <strong><mark>';
-    var description5 = '</strong></mark> colony forming units (CFUs).';
+    var description4 = '3. After 3 days of anaerobic incubation, your plate has <b>';
+    var description5 = '</b> colony forming units (CFUs).';
     document.getElementById("cfu_description").innerHTML = description4 + random4 + description5;
-    cfu_final = random4 * plated;
-    cfu_original = random4 / amt;
-    document.getElementById("answer3a").innerHTML = cfu_final.toString();
-    document.getElementById("answer3b").innerHTML = cfu_original.toString();
+    answer_3a = random4 * plated;
+    answer_3b = random4 / amt;
+    answer_3a = answer_3a.toString();
+    answer_3b = answer_3b.toString();
+
+    // Settings
     document.getElementById('q4a').value = "none";
     document.getElementById("quest_4b").innerHTML = "";
     document.getElementById("quest_4c").innerHTML = "";
-    document.getElementById("answer4").innerHTML = "";
-    document.getElementById("answer5").innerHTML = "";
+    document.getElementById("answer4b").innerHTML = "";
+    document.getElementById("answer4c").innerHTML = "";
+    document.getElementById('button4b').style.display = "none";
+    document.getElementById('button4c').style.display = "none";
+    button1_value = 0;
+    button2_value = 0;
+    button3a_value = 0;
+    button3b_value = 0;
+    button4b_value = 0;
+    button4c_value = 0;
+    document.getElementById("answer1").innerHTML = "";
+    document.getElementById("answer2").innerHTML = "";
+    document.getElementById("answer3a").innerHTML = "";
+    document.getElementById("answer3b").innerHTML = "";
+    document.getElementById("answer4b").innerHTML = "";
+    document.getElementById("answer4c").innerHTML = "";
 }
 
 // Question 4b
 function display_4b() {
     var x = document.getElementById('q4a').value;
+    document.getElementById('question_4').classList = "show";
     if (x == 'none' || x == 'acc') {
-        new_cfu = NaN;
+        answer_4c = NaN;
+        document.getElementById('button4b').style.display = "none";
+        document.getElementById('button4c').style.display = "none";
         document.getElementById("quest_4b").innerHTML = "";
         document.getElementById("quest_4c").innerHTML = "";
-        document.getElementById("answer4").innerHTML = "";
-        document.getElementById("answer5").innerHTML = "";
+        document.getElementById("answer4b").innerHTML = "";
+        document.getElementById("answer4c").innerHTML = "";
     } else {
-        document.getElementById("quest_4b").innerHTML = "<h4>4b. What is the expected CFU/ml of this serial dilution? <em>Round to the nearest integer.</em></h4>";
-        document.getElementById("quest_4c").innerHTML = "<h4>4c. What is the expected CFU of this serial dilution after plating? <em>Round to the nearest integer and assume that the same volume is seeded.</em></h4>";
+        document.getElementById('button4b').style.display = "inline";
+        document.getElementById('button4c').style.display = "inline";
+        document.getElementById("quest_4b").innerHTML = "4b. What is the expected CFU/ml of this serial dilution? <em>Round to the nearest integer.</em><br>";
+        document.getElementById("quest_4c").innerHTML = "4c. What is the expected CFU of this serial dilution after plating? <em>Round to the nearest integer and assume that the same volume is seeded.</em><br>";
         if (x == 'more') {
-            var new_conc = (random4 / amt) / split;
-            new_cfu = random4 / split;
+            answer_4b = (random4 / amt) / split;
+            answer_4c = random4 / split;
         } else if (x == 'less') {
-            var new_conc = (random4 / amt) * split;
-            new_cfu = random4 * split;
+            answer_4b = (random4 / amt) * split;
+            answer_4c = random4 * split;
         }
-        var new_conc = Math.round(new_conc);
-        new_cfu = Math.round(new_cfu);
-        document.getElementById("answer4").innerHTML = new_conc.toString();
-        document.getElementById("answer5").innerHTML = new_cfu.toString();
+        answer_4b = Math.round(answer_4b);
+        answer_4c = Math.round(answer_4c);
+        answer_4b = answer_4b.toString();
+        answer_4c = answer_4c.toString();
     }
     number_line();
     document.getElementById('visualizer').classList = "show";
@@ -102,7 +134,7 @@ function number_line() {
 
     var updated = {
         y: [''],
-        x: [new_cfu],
+        x: [answer_4c],
         name: 'New CFU',
         type: 'scatter',
         mode: 'markers',
@@ -201,4 +233,59 @@ function number_line() {
     };
 
     Plotly.newPlot('visualizer', data, layout, { scrollZoom: true });
+}
+
+function button1_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer1").innerHTML = answer_1;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer1").innerHTML = "";
+      button1_value = 0;
+    }
+}
+function button2_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer2").innerHTML = answer_2;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer2").innerHTML = "";
+      button1_value = 0;
+    }
+}
+function button3a_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer3a").innerHTML = answer_3a;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer3a").innerHTML = "";
+      button1_value = 0;
+    }
+}
+function button3b_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer3b").innerHTML = answer_3b;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer3b").innerHTML = "";
+      button1_value = 0;
+    }
+}
+function button4b_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer4b").innerHTML = answer_4b;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer4b").innerHTML = "";
+      button1_value = 0;
+    }
+}
+function button4c_onclick(){
+    if(button1_value == 0 ) {
+      document.getElementById("answer4c").innerHTML = answer_4c;
+      button1_value = 1;
+    } else {
+      document.getElementById("answer4c").innerHTML = "";
+      button1_value = 0;
+    }
 }
